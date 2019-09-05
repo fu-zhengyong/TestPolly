a2dfd0c9c4beedf39fb64583062713ba6c6de51c
fuzy <fxy6781349@qq.com>
2019-09-05 11:24:11 +0800

    提交初始化

---
+++ TestPolly/TestPolly/Controllers/ClientController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestPolly.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly ProductService productService;
+
+        public ClientController(ProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<string> Get(int id)
+        {
+            var product = await productService.GetAllProductsAsync("B");
+
+            return product;
+        }
+    }
+}
---
+++ TestPolly/TestPolly/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AspectCore.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TestPolly.Utils;
+
+namespace TestPolly
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            RegisterServices(this.GetType().Assembly, services);
+            return services.BuildAspectInjectorProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        private static void RegisterServices(Assembly asm, IServiceCollection services)
+        {
+            foreach (var type in asm.GetExportedTypes())
+            {
+                bool hasHystrixCommand = type.GetMethods().Any(m =>
+                    m.GetCustomAttribute(typeof(HystrixCommandAttribute)) != null);
+                if (hasHystrixCommand)
+                {
+                    services.AddSingleton(type);
+                }
+            }
+        }
+    }
+}
